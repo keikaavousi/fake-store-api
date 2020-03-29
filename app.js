736aea98d0e5f97cd85a23d01cfb66b913bfb4ab
MohammadReza Keikavousi <keikaavousi@gmail.com>
2020-03-29 15:44:29 +0430

    change monogodb db name

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use('/',(req,res)=>{
 
 //mongoose
 mongoose.set('useFindAndModify',false)
-mongoose.connect('mongodb+srv://keikaavousi:qaqjob-rovjy3-pucSaq@cluster0-ffwd2.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser:true})
+mongoose.connect('mongodb+srv://keikaavousi:qaqjob-rovjy3-pucSaq@cluster0-ffwd2.mongodb.net/shop?retryWrites=true&w=majority',{useNewUrlParser:true})
 .then(result=>{
     app.listen(process.env.PORT || port,()=>{
         console.log('connected!')
