5e4e0d71327e9830efb62fc48065761eb3dfed63
MohammadReza Keikavousi <keikaavousi@gmail.com>
2020-03-30 13:18:36 +0430

    add some other controllers,routes for search and readme

---
+++ README.txt
@@ -0,0 +1,36 @@
+GET:
+/products                                           get all products
+/products/1                                         get specific product based on id
+/products?limit=5                                   limit return results 
+/products?sort=desc                                 asc|desc  get products in ascending or descending orders (default to asc)
+/products/category/jewelery                         get all products in specific category
+/products/category/jewelery?sort=desc               asc|desc  get products in ascending or descending orders (default to asc)
+
+POST:
+/products                                          post dummy product, you can post a product with this schema:
+    title:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    description:{
+        type:String
+    },
+    image:{
+        type:String
+    },
+    category:{
+        type:String
+    }
+    if it will post correctly, it will return a fake id:21 with your product object sent.
+    remember that your data won't insert into our database and if you refresh your page (eg. in web applications) it will no longer in fetched datas.              
+
+PUT:
+/products                   edit a product, you should send an id and it will return you the selected product. remember that this api only work with id passed, otherwise it will return an error.
+
+
+DELETE:
+/products                   delete a product, you should send an id and it will return you the selected product. remember that this api only work with id passed, otherwise it will return an error.
--- controller/product.js
+++ controller/product.js
@@ -1,56 +1,49 @@
-const Product = require("../model/product");
+const Product = require("../model/product")
 
 module.exports.getAllProducts = (req, res) => {
-  Product.find()
+  const limit = Number(req.query.limit) || 0
+  const sort = req.query.sort=="desc"?-1:1
+
+  Product.find().limit(limit).sort({id:sort})
     .then(products => {
-      res.json(products);
+      res.json(products)
     })
-    .catch(err => console.log(err));
-
-
-  //  const product = new Product({
-  //     id: 21,
-  //     title: 'rerer',
-  //     price: 12.45,
-  //     description: 'fef',
-  //     image: 'sdgg/dsg/',
-  //     category: 'fdsgg'
-  //   })
-  //      product.save()
-  //   .then(res=>{
-  //     console.log(res)
-  //   })
-  //   .catch(err=>{
-  //     console.log(err)
-  //   })
-};
+    .catch(err=> console.log(err))
+}
 
 module.exports.getProduct = (req, res) => {
-  const id = req.params.id;
+  const id = req.params.id
+  
   Product.findOne({
-    id:id
+    id
   })
-    .then(product => {
-      res.json(product);
+    .then(products => {
+      res.json(products)
     })
-    .catch(err => console.log(err));
-};
+    .catch(err=> console.log(err))
+}
+
+module.exports.getProductsInCategory = (req,res) => {
+  const category = req.params.category
+  const limit = Number(req.query.limit) || 0
+  const sort = req.query.sort=="desc"?-1:1
+
+  Product.find({
+    category
+  }).limit(limit).sort({id:sort})
+  .then(products => {
+    res.json(products)
+  })
+  .catch(err=> console.log(err))
+}
 
 module.exports.addProduct = (req, res) => {
   if (typeof req.body == undefined) {
     res.json({
       status: "error",
       message: "data is undefined"
-    });
+    })
   } else {
-    // const product = new Product({
-    //   id: 21,
-    //   title: req.body.title,
-    //   price: req.body.price,
-    //   description: req.body.description,
-    //   image: req.body.image,
-    //   category: req.body.category
-    // })
     res.json({
       id: 21,
       title: req.body.title,
@@ -58,23 +51,16 @@ module.exports.addProduct = (req, res) => {
       description: req.body.description,
       image: req.body.image,
       category: req.body.category
-    });
-    // product.save()
-    // .then(res=>{
-    //   console.log(res)
-    // })
-    // .catch(err=>{
-    //   console.log(err)
-    // })
+    })
   }
-};
+}
 
 module.exports.editProduct = (req, res) => {
   if (typeof req.body == undefined || req.body.id == null) {
     res.json({
       status: "error",
       message: "something went wrong! check your sent data"
-    });
+    })
   } else {
     res.json({
       id: req.body.id,
@@ -83,16 +69,16 @@ module.exports.editProduct = (req, res) => {
       description: req.body.description,
       image: req.body.image,
       category: req.body.category
-    });
+    })
   }
-};
+}
 
 module.exports.deleteProduct = (req, res) => {
     if (typeof req.body == undefined || req.body.id == null) {
         res.json({
           status: "error",
           message: "something went wrong! check your sent data"
-        });
+        })
       } else {
       Product.findById(req.params.id)
       .then(product=>{
@@ -103,7 +89,7 @@ module.exports.deleteProduct = (req, res) => {
             description: req.body.description,
             image: req.body.image,
             category: req.body.category
-          });
+          })
       })
     }
-  };
+  }
