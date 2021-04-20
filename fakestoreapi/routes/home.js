faaf26d94a7ae621ec9e5fbff7dfc47028f016cb
Mohammad Reza Keikaavousi <keikaavousi@gmail.com>
2021-04-20 21:24:55 +0430

    update files

--- fakestoreapi/routes/home.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const home = require('../controller/home')
-
-router.get('/',home.indexPage)
-router.get('/docs',home.docsPage)
-
-module.exports = router
