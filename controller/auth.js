063135b817c60e1d635b2e6a93cb3e934aab661f
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-06-06 06:21:03 +0700

    Destruc request body, implement async await funct, update module.exports

--- controller/auth.js
+++ controller/auth.js
@@ -1,26 +1,27 @@
-const User = require('../model/user');
-const jwt = require('jsonwebtoken');
+const User = require("../model/user");
+const jwt = require("jsonwebtoken");
 
-module.exports.login = (req, res) => {
-	const username = req.body.username;
-	const password = req.body.password;
-	if (username && password) {
-		User.findOne({
-			username: username,
-			password: password,
-		})
-			.then((user) => {
-				if (user) {
-					res.json({
-						token: jwt.sign({ user: username }, 'secret_key'),
-					});
-				} else {
-					res.status(401);
-					res.send('username or password is incorrect');
-				}
-			})
-			.catch((err) => {
-				console.error(err);
-			});
-	}
+module.exports = {
+  login: async (req, res) => {
+    const { username, password } = req.body;
+    try {
+      if (username && password) {
+        const user = await User.findOne({
+          username: username,
+          password: password,
+        });
+
+        if (user) {
+          res.json({
+            token: jwt.sign({ user: username }, "secret_key"),
+          });
+        } else {
+          res.status(401);
+          res.send("username or password is incorrect");
+        }
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  },
 };
